4f1c6038c9a7d3e2ba443718fb858ffa3f3f8d74
JulioAPeraza <jpera054@fiu.edu>
2024-01-15 17:41:13 -0500

    Update env

--- env/create_env.sh
+++ env/create_env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 module add miniconda3-4.5.11-gcc-8.2.0-oqs2mbg
-conda create -p /home/data/nbc/misc-projects/Peraza_large-scale-ibma/env/conda_env pip python=3.10 -y
+conda create -p /home/data/nbc/misc-projects/Peraza_large-scale-ibma/env/nv-ibma_env pip python=3.10 -y
 
 conda config --append envs_dirs /home/data/nbc/misc-projects/Peraza_large-scale-ibma/env
 
-source activate /home/data/nbc/misc-projects/Peraza_large-scale-ibma/env/conda_env
+source activate /home/data/nbc/misc-projects/Peraza_large-scale-ibma/env/nv-ibma_env
 
-pip install black flake8 isort nimare ipykernel seaborn
+pip install -r /home/data/nbc/misc-projects/Peraza_large-scale-ibma/requirements.txt
