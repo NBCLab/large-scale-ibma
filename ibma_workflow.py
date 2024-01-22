8421932bccc15ce9538666634ef83a81280c5cf0
JulioAPeraza <jpera054@fiu.edu>
2024-01-22 17:18:30 -0500

    Update IBMA workflow

--- ibma_workflow.py
+++ ibma_workflow.py
@@ -23,6 +23,12 @@ def _get_parser():
         help="Path to project directory",
     )
     parser.add_argument(
+        "--dset_type",
+        dest="dset_type",
+        required=True,
+        help="Kind of dataset to use",
+    )
+    parser.add_argument(
         "--job_id",
         dest="job_id",
         required=True,
@@ -76,45 +82,52 @@ def _exclude_outliers(dset):
     return dset.slice(unique_ids)
 
 
-def main(project_dir, job_id, n_cores):
+def main(project_dir, dset_type, job_id, n_cores):
     project_dir = op.abspath(project_dir)
     job_id = int(job_id)
     n_cores = int(n_cores)
-    
+
     result_dir = op.join(project_dir, "results")
     cogat_df = pd.read_csv(op.join(project_dir, "data", "cogat_terms.csv"))
 
     task_names = cogat_df["cogat_nm"].tolist()
     task_name = task_names[job_id]  # Parallelize using job_id in Slurm
 
-    print(f"Running task {job_id}/{len(task_names)}: {task_name}")
+    print(f"Running task {job_id+1}/{len(task_names)}: {task_name}", flush=True)
     output_dir = op.join(result_dir, "ibma", task_name)
     images_dir = op.join(project_dir, "data", "images", task_name)
+    report_dir = op.join(output_dir, f"report-{dset_type}")
     os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(report_dir, exist_ok=True)
 
-    z_dset_fn = op.join(output_dir, f"{task_name}_dset.pkl.gz")  # At least this must exist
+    z_dset_dict = {}
+    z_dset_fn = op.join(output_dir, f"{task_name}_dset-raw.pkl.gz")  # At least this must exist
     z_dset = Dataset.load(z_dset_fn)
     z_dset.update_path(images_dir)
+    z_dset_dict["raw"] = z_dset
 
-    z_dset_clean_fn = op.join(output_dir, f"{task_name}_dset-clean.pkl.gz")
-    if not op.isfile(z_dset_clean_fn):
-        z_dset_clean = _exclude_outliers(z_dset)
-        z_dset_clean.save(z_dset_clean_fn)
-    else:
-        z_dset_clean = Dataset.load(z_dset_clean_fn)
+    if dset_type == "clean":
+        z_dset_clean_fn = op.join(output_dir, f"{task_name}_dset-clean.pkl.gz")
+        if not op.isfile(z_dset_clean_fn):
+            z_dset_clean = _exclude_outliers(z_dset)
+            z_dset_clean.save(z_dset_clean_fn)
+        else:
+            z_dset_clean = Dataset.load(z_dset_clean_fn)
 
-    print(f"Included Studies: {len(z_dset_clean.ids)}/{len(z_dset.ids)}")
+        z_dset_dict["clean"] = z_dset_clean
 
-    result_fn = op.join(output_dir, f"{task_name}_result-corr.pkl.gz")
+        print(f"Included Studies: {len(z_dset_clean.ids)}/{len(z_dset.ids)}", flush=True)
+
+    result_fn = op.join(output_dir, f"{task_name}_result-{dset_type}.pkl.gz")
     if not op.isfile(result_fn):
-        workflow = IBMAWorkflow(estimator=Stouffers(aggressive_mask=False))
-        result = workflow.fit(z_dset_clean)
+        workflow = IBMAWorkflow(estimator=Stouffers(aggressive_mask=False), n_cores=n_cores)
+        result = workflow.fit(z_dset_dict[dset_type])
         result.save(result_fn)
     else:
         result = MetaResult.load(result_fn)
 
-    if not op.isfile(op.join(output_dir, "report.html")):
-        run_reports(result, output_dir)
+    if not op.isfile(op.join(report_dir, "report.html")):
+        run_reports(result, report_dir)
 
 
 def _main(argv=None):
