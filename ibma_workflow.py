151cfe59b25b93e33ddd00e923f58ae34b99ca19
JulioAPeraza <jpera054@fiu.edu>
2024-01-29 15:07:28 -0500

    Exclude with all positive or all negative values and with less than 40% of voxels

--- ibma_workflow.py
+++ ibma_workflow.py
@@ -45,19 +45,35 @@ def _get_parser():
 
 
 def _get_outliers(data, ids):
-    n_studies = data.shape[0]
+    # Here n_voxels is data.shape[1] considering the nonaggresive mask is used
+    n_studies, n_voxels = data.shape
 
     mask = ~np.isnan(data) & (data != 0)
     maps_lst = [data[i][mask[i]] for i in range(n_studies)]
 
-    scores = [np.var(map_) for map_ in maps_lst]
-
+    # Get outliers based on variance within each study
+    # Set var of empty maps to 0, it will be remove by perc_outliers
+    scores = [np.var(map_) if len(map_) > 1 else 0 for map_ in maps_lst]
     q1 = np.percentile(scores, 25)
     q3 = np.percentile(scores, 75)
     iqr = q3 - q1
-
     threshold = 1.5 * iqr
-    outliers_idxs = np.where((scores < q1 - threshold) | (scores > q3 + threshold))
+    var_outliers = np.where((scores < q1 - threshold) | (scores > q3 + threshold))[0]
+
+    # Get images with all positive or all negative values
+    oth_outliers = np.array(
+        [
+            map_i
+            for map_i, map_ in enumerate(maps_lst)
+            if ((map_ > 0).sum() == len(map_)) or ((map_ < 0).sum() == len(map_))
+        ]
+    )
+
+    # Get images with less than 40% of voxels
+    perc_voxs = mask.sum(axis=1) / n_voxels
+    perc_outliers = np.where(perc_voxs < 0.4)[0]
+
+    outliers_idxs = np.unique(np.hstack([var_outliers, oth_outliers, perc_outliers]))
 
     return ids[outliers_idxs]
 
@@ -67,9 +83,11 @@ def _exclude_outliers(dset):
     masker = dset.masker
 
     imgs = [
-        nib.load(img)
-        if _check_same_fov(nib.load(img), reference_masker=masker.mask_img)
-        else resample_to_img(nib.load(img), masker.mask_img)
+        (
+            nib.load(img)
+            if _check_same_fov(nib.load(img), reference_masker=masker.mask_img)
+            else resample_to_img(nib.load(img), masker.mask_img)
+        )
         for img in images
     ]
 
