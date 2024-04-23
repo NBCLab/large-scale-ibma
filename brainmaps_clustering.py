a27baab5d1ae8c902d59caaf12c968fd463b7551
JulioAPeraza <jpera054@fiu.edu>
2024-04-23 11:12:07 -0400

    Update brainmaps_clustering.py

--- brainmaps_clustering.py
+++ brainmaps_clustering.py
@@ -24,6 +24,14 @@ import numpy as np
 
 from utils import _exclude_outliers
 
+# Set up dimensionality reduction and clustering parameters
+# n_neighbors range from 2 to a quarter of the data
+# 0.0 is the recommended value for finding clusters (smallest min_dist)
+COMPONENTS = range(2, 7)
+NEIGHBORS = np.linspace(5, 100, 5, dtype=int)
+DISTANCES = [0.0, 0.1, 0.25, 0.5, 0.8]
+SAMPLES = np.linspace(2, 30, 5, dtype=int)  # min_samples for HDBSCAN
+
 
 def _get_parser():
     parser = argparse.ArgumentParser(description="Get clusters in NeuroVault data")
@@ -46,6 +54,19 @@ def _get_parser():
         help="Name of the atlas to use for feature extraction",
     )
     parser.add_argument(
+        "--space",
+        dest="space",
+        required=False,
+        help="Name of the space to use for feature extraction",
+    )
+    parser.add_argument(
+        "--tasks",
+        dest="tasks",
+        required=False,
+        nargs="+",
+        help="Name of the space to use for feature extraction",
+    )
+    parser.add_argument(
         "--n_cores",
         dest="n_cores",
         default=1,
@@ -55,10 +76,14 @@ def _get_parser():
     return parser
 
 
-def _rm_nonstat_maps(dset, data_df):
+def _rm_nonstat_maps(dset):
     """
     Remove non-statistical maps from a dataset.
     """
+    data_df = dset.metadata
+
+    assert "image_name" in data_df.columns
+
     ids_to_keep = []
     for _, row in data_df.iterrows():
         image_name = row["image_name"]
@@ -99,19 +124,19 @@ def _get_features_from_imgs(images, masker, atlas=None):
         data = masker_difumo.fit_transform(img4d)
     elif atlas == "schaefer":
         schaefer = datasets.fetch_atlas_schaefer_2018(n_rois=100, resolution_mm=2)
-        masker_schaefer = NiftiLabelsMasker(labels_img=schaefer.maps, standardize="zscore")
+        masker_schaefer = NiftiLabelsMasker(labels_img=schaefer.maps, standardize="zscore_sample")
 
         data = masker_schaefer.fit_transform(img4d)
     elif atlas == "smith":
         smith = datasets.fetch_atlas_smith_2009()
-        masker_smith = NiftiLabelsMasker(labels_img=smith["rsn20"], standardize="zscore")
+        masker_smith = NiftiLabelsMasker(labels_img=smith["rsn20"], standardize="zscore_sample")
 
         data = masker_smith.fit_transform(img4d)
     elif atlas == "power":
         power = datasets.fetch_coords_power_2011()
         coords = np.vstack((power.rois["x"], power.rois["y"], power.rois["z"])).T
 
-        masker_power = NiftiSpheresMasker(seeds=coords, radius=10, standardize="zscore")
+        masker_power = NiftiSpheresMasker(seeds=coords, radius=5, standardize="zscore_sample")
 
         data = masker_power.fit_transform(img4d)
     else:
@@ -120,60 +145,44 @@ def _get_features_from_imgs(images, masker, atlas=None):
     return data
 
 
-def main(project_dir, database="neurovault", atlas=None, n_cores=1):
+def main(project_dir, database="neurovault", atlas=None, space="umap", tasks=None, n_cores=1):
     data_dir = op.join(project_dir, "data")
     results_dir = op.join(project_dir, "results")
     clustering_dir = op.join(results_dir, f"{database}_clustering")
     n_cores = int(n_cores)
     os.makedirs(clustering_dir, exist_ok=True)
 
-    dset = Dataset.load(op.join(results_dir, "neurovault_full_dataset.pkl"))
+    dset = Dataset.load(op.join(data_dir, "neurovault_full_dataset.pkl"))
     masker = dset.masker
 
     # Initialize DataFrame to store results
     data_df = pd.DataFrame()
 
     if database == "neurovault":
-        nv_collections_images_df = pd.read_csv(op.join(data_dir, "nv_collections_images.csv"))
+        if tasks:
+            id_sel = dset.metadata[dset.metadata["cognitive_paradigm_cogatlas_id"].isin(tasks)][
+                "id"
+            ].values
+            dset = dset.slice(id_sel)
 
-        # For now, we will only use the following tasks
-        sel_task = [
-            "trm_4f2453ce33f16",
-            "tsk_Ncknr0soiM4IV",
-            "tsk_4a57abb949e8a",
-            "tsk_mFS3uwUMAhXxe",
-            "tsk_4a57abb949a93",
-            "trm_4f23fc8c42d28",
-        ]
-        id_sel = dset.metadata[dset.metadata["cognitive_paradigm_cogatlas_id"].isin(sel_task)][
-            "id"
-        ].values
+        dset = _rm_nonstat_maps(dset)
+        dset = _exclude_outliers(dset)
 
-        dset = _rm_nonstat_maps(dset, nv_collections_images_df)
-        dset_sel = dset.slice(id_sel)
-        dset_sel_clean = _exclude_outliers(dset_sel)
-
-        data_df["id"] = dset_sel_clean.images["id"]
-        data_df = pd.merge(data_df, nv_collections_images_df, how="left", on="id")
-        data_df["pmid"] = data_df["pmid"].astype(str)
+        data_df["id"] = dset.images["id"]
+        data_df = pd.merge(data_df, dset.metadata, how="left", on="id")
 
         # Get features from dset given an atlas
-        images = dset_sel_clean.get_images(imtype="z")
+        images = dset.get_images(imtype="z")
 
     elif database == "neurosynth":
         metamaps_dir = op.join(data_dir, "neurosynth", "metamaps")
 
-        img_paths = sorted(glob(os.path.join(metamaps_dir, "*.nii*")))
-        img_names = [os.path.basename(img).split(os.extsep)[0] for img in img_paths]
-        feature_imgs_dict = dict(zip(img_names, img_paths))
-
-        features, images = ([], [])
-        for feature, img_path in feature_imgs_dict.items():
-            img = nib.load(img_path)
-            features.append(feature)
-            images.append(np.squeeze(masker.transform(img)))
+        images = sorted(glob(os.path.join(metamaps_dir, "*.nii*")))
+        features = [os.path.basename(img).split(os.extsep)[0] for img in images]
 
         data_df["feature"] = features
+    else:
+        raise ValueError("Invalid database")
 
     data = _get_features_from_imgs(images, masker, atlas=atlas)
 
@@ -186,56 +195,59 @@ def main(project_dir, database="neurovault", atlas=None, n_cores=1):
     np.save(op.join(clustering_dir, f"{atlas}_aff.npy"), affinity_data)
     np.save(op.join(clustering_dir, f"{atlas}_dis.npy"), distance_data)
 
-    # Set up dimensionality reduction and clustering parameters
-    # n_neighbors range from 2 to a quarter of the data
-    # 0.0 is the recommended value for finding clusters (smallest min_dist)
-    components = range(2, 7)
-    neighbors = np.linspace(5, 100, 5, dtype=int)
-    distances = [0.0, 0.1, 0.25, 0.5, 0.8]
-    samples = np.linspace(2, 30, 5, dtype=int)
-    silhouette_dict = {"min_samples": [], "params": [], "score": []}
-    for n_components, n_neighbors, min_dist in itertools.product(components, neighbors, distances):
-        reduce_nv = umap.UMAP(
-            n_neighbors=n_neighbors,
-            min_dist=min_dist,
-            n_components=n_components,
-            metric="correlation",
-            n_jobs=n_cores,
-        )
-        mapper_nv = reduce_nv.fit(data)
-        embedding_nv = mapper_nv.transform(data)
+    if space == "umap":
+        # silhouette_dict = {"min_samples": [], "params": [], "score": []}
+        for n_components, n_neighbors, min_dist in itertools.product(
+            COMPONENTS, NEIGHBORS, DISTANCES
+        ):
+            print(n_components, n_neighbors, min_dist)
 
-        columns = [
-            f"c-{n_components}-{i}_n-{n_neighbors}_d-{min_dist}" for i in range(n_components)
-        ]
-        embeding_df = pd.DataFrame(embedding_nv, columns=columns)
-        data_df = pd.concat([data_df, embeding_df], axis=1)
+            reduce_nv = umap.UMAP(
+                n_neighbors=n_neighbors,
+                min_dist=min_dist,
+                n_components=n_components,
+                metric="correlation",
+                n_jobs=n_cores,
+            )
+            mapper_nv = reduce_nv.fit(data)
+            embedding_nv = mapper_nv.transform(data)
 
-        print(n_components, n_neighbors, min_dist)
-        for min_samples in samples:
-            print(min_samples)
+            columns = [
+                f"c-{n_components}-{i}_n-{n_neighbors}_d-{min_dist}" for i in range(n_components)
+            ]
+            embeding_df = pd.DataFrame(embedding_nv, columns=columns)
+            data_df = pd.concat([data_df, embeding_df], axis=1)
+
+            for min_samples in SAMPLES:
+                labels = hdbscan.HDBSCAN(
+                    min_samples=min_samples,
+                    min_cluster_size=10,
+                ).fit_predict(embedding_nv)
+
+                labels = labels.astype(str)
+                data_df[f"hdbscan_c-{n_components}_n-{n_neighbors}_d-{min_dist}_{min_samples}"] = (
+                    labels
+                )
+
+                # silhouette = silhouette_score(embedding_nv, labels)
+
+                # silhouette_dict["min_samples"].append(min_samples)
+                # silhouette_dict["params"].append(f"c-{n_components}_n-{n_neighbors}_d-{min_dist}")
+                # silhouette_dict["score"].append(silhouette)
+
+        # silhouette_df = pd.DataFrame(silhouette_dict)
+        # silhouette_df.to_csv(op.join(clustering_dir, f"{atlas}_silhouette.csv"))
+    elif space == "full":
+        for min_samples in SAMPLES:
             labels = hdbscan.HDBSCAN(
                 min_samples=min_samples,
                 min_cluster_size=10,
-            ).fit_predict(embedding_nv)
+            ).fit_predict(data)
 
-            data_df[f"hdbscan_c-{n_components}_n-{n_neighbors}_d-{min_dist}_{min_samples}"] = (
-                labels
-            )
-            data_df[f"hdbscan_c-{n_components}_n-{n_neighbors}_d-{min_dist}_{min_samples}"] = (
-                data_df[
-                    f"hdbscan_c-{n_components}_n-{n_neighbors}_d-{min_dist}_{min_samples}"
-                ].astype(str)
-            )
-            silhouette = silhouette_score(embedding_nv, labels)
+            labels = labels.astype(str)
+            data_df[f"hdbscan_{min_samples}"] = labels
 
-            silhouette_dict["min_samples"].append(min_samples)
-            silhouette_dict["params"].append(f"c-{n_components}_n-{n_neighbors}_d-{min_dist}")
-            silhouette_dict["score"].append(silhouette)
-
-    silhouette_df = pd.DataFrame(silhouette_dict)
-    silhouette_df.to_csv(op.join(clustering_dir, f"{atlas}_silhouette.csv"))
-    data_df.to_csv(op.join(clustering_dir, f"{atlas}_data.csv"))
+    data_df.to_csv(op.join(clustering_dir, f"{space}-{atlas}_data.csv"))
 
 
 def _main(argv=None):
