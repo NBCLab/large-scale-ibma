955d9119515436fe6fa73f0ea8f13e8aaf4f8542
JulioAPeraza <julio.peraza@outlook.com>
2024-09-17 09:23:29 -0700

    Add IBMA workflow with Hedges' g

---
+++ ibma.py
@@ -0,0 +1,148 @@
+import logging
+import math
+
+import numpy as np
+from nilearn.input_data import NiftiMasker
+from nimare.meta.ibma import IBMAEstimator
+from nimare.transforms import d_to_g, t_to_d, z_to_p
+from nimare.utils import _boolean_unmask
+from scipy.stats import norm
+
+LGR = logging.getLogger(__name__)
+
+
+def calculate_means(estimates, n_maps, gamma=0.2, method="mean"):
+    K = estimates.shape[0]  # K samples, V voxels
+
+    if method == "mean":
+        est_maps = estimates.mean(axis=0)
+        mean_absolute_deviation = np.abs(estimates - est_maps).mean(axis=0)
+        var_maps = mean_absolute_deviation**2
+
+        var_mean_maps = (1 / n_maps[0, :]) * var_maps
+        return est_maps, var_mean_maps
+
+    elif method == "median":
+        est_maps = np.median(estimates, axis=0)
+        median_absolute_deviation = np.median(np.abs(estimates - est_maps), axis=0) * (
+            1 / norm.ppf(3 / 4)
+        )
+        var_maps = median_absolute_deviation**2
+
+        var_mean_maps = (math.pi / (2 * n_maps[0, :])) * var_maps
+        return est_maps, var_mean_maps
+
+    elif (method == "trimmed") or (method == "winsorized"):
+        K_gamma = int(gamma * K / 2)
+
+        # Sort the estimates along each voxel
+        estimates_sorted = np.sort(estimates, axis=0)
+
+        # Trimmed mean calculation
+        estimates_trimmed = estimates_sorted[K_gamma:-K_gamma, :]
+        trimmed_mean = np.mean(estimates_trimmed, axis=0)
+
+        # Windsorized mean calculation
+        estimates_winsorized = estimates_sorted.copy()
+        estimates_winsorized[:K_gamma, :] = estimates_sorted[K_gamma, :]
+        estimates_winsorized[-K_gamma:, :] = estimates_sorted[-K_gamma - 1, :]
+        winsorized_mean = np.mean(estimates_winsorized, axis=0)
+
+        # Windsorized estimate of data variance
+        winsorized_var = (
+            K_gamma * (estimates_sorted[K_gamma, :] - winsorized_mean) ** 2
+            + np.sum((estimates_winsorized[K_gamma:-K_gamma, :] - winsorized_mean) ** 2, axis=0)
+            + K_gamma * (estimates_sorted[-K_gamma - 1, :] - winsorized_mean) ** 2
+        ) / (K - 1)
+
+        if method == "trimmed":
+            var_mean_maps = (1 / (n_maps[0, :] - 2 * K_gamma)) * winsorized_var
+            return trimmed_mean, var_mean_maps
+        elif method == "winsorized":
+            var_mean_maps = (1 / n_maps[0, :]) * winsorized_var
+            return winsorized_mean, var_mean_maps
+    else:
+        raise ValueError(f"Method {method} not recognized.")
+
+
+class AverageHedges(IBMAEstimator):
+    _required_inputs = {"t_maps": ("image", "t"), "sample_sizes": ("metadata", "sample_sizes")}
+
+    def __init__(self, gamma=0.2, method="mean", **kwargs):
+        super().__init__(**kwargs)
+        self.gamma = gamma
+        self.method = method
+
+    def _generate_description(self):
+        description = (
+            f"An image-based meta-analysis was performed with NiMARE"
+            "(RRID:SCR_017398; \\citealt{Salo2023}), on "
+            f"{len(self.inputs_['id'])} t-statistic images using Heges' g as point estimates "
+            "and the variance of bias-corrected Cohen's in a Weighted Least Squares approach "
+            "\\citep{brockwell2001comparison,bossier2019}, "
+            f"with an a priori tau-squared value of defined across all voxels."
+        )
+        return description
+
+    def _fit_model(self, t_maps, study_mask=None):
+        """Fit the model to the data."""
+        n_studies, n_voxels = t_maps.shape
+
+        if study_mask is None:
+            # If no mask is provided, assume all studies are included. This is always the case
+            # when using the aggressive mask.
+            study_mask = np.arange(n_studies)
+
+        sample_sizes = np.array([np.mean(self.inputs_["sample_sizes"][idx]) for idx in study_mask])
+        n_maps = np.tile(sample_sizes, (n_voxels, 1)).T
+
+        # Calculate Hedge's g maps: Standardized mean
+        cohens_maps = t_to_d(t_maps, n_maps)
+        hedges_maps = d_to_g(cohens_maps, n_maps)
+
+        del sample_sizes, cohens_maps
+
+        est_maps, var_mean_maps = calculate_means(
+            hedges_maps,
+            n_maps,
+            gamma=self.gamma,
+            method=self.method,
+        )
+
+        z_map = est_maps / np.sqrt(var_mean_maps)
+        p_map = z_to_p(z_map)
+
+        return z_map, p_map, est_maps
+
+    def _fit(self, dataset):
+        self.dataset = dataset
+        self.masker = self.masker or dataset.masker
+        if not isinstance(self.masker, NiftiMasker):
+            LGR.warning(
+                f"A {type(self.masker)} mask has been detected. "
+                "Masks which average across voxels will likely produce biased results when used "
+                "with this Estimator."
+            )
+
+        if self.aggressive_mask:
+            voxel_mask = self.inputs_["aggressive_mask"]
+            result_maps = self._fit_model(self.inputs_["t_maps"][:, voxel_mask])
+
+            z_map, p_map, est_map = tuple(
+                map(lambda x: _boolean_unmask(x, voxel_mask), result_maps)
+            )
+        else:
+            n_voxels = self.inputs_["t_maps"].shape[1]
+
+            z_map, p_map, est_map = [np.zeros(n_voxels, dtype=float) for _ in range(3)]
+            for bag in self.inputs_["data_bags"]["t_maps"]:
+                (
+                    z_map[bag["voxel_mask"]],
+                    p_map[bag["voxel_mask"]],
+                    est_map[bag["voxel_mask"]],
+                ) = self._fit_model(bag["values"], bag["study_mask"])
+
+        maps = {"z": z_map, "p": p_map, "est": est_map}
+        description = self._generate_description()
+
+        return maps, {}, description
---
+++ ibma_hedges_workflow.py
@@ -0,0 +1,148 @@
+import argparse
+import os
+import os.path as op
+import warnings
+
+from nimare.dataset import Dataset
+from nimare.diagnostics import Jackknife
+from nimare.meta.ibma import FixedEffectsHedges
+from nimare.workflows import IBMAWorkflow
+
+from ibma import AverageHedges
+from utils import _exclude_outliers, _rm_nonstat_maps
+
+warnings.filterwarnings("ignore")
+
+TASK_IDS = {
+    "working_memory": [
+        "trm_550b50095d4a3",  # working memory fMRI task paradigm
+        "tsk_4a57abb949a0d",  # digit span task
+        "tsk_4a57abb949bcd",  # n-back task
+    ],
+}
+MANUAL_SELECTION = {
+    "working_memory": [
+        59429,
+        57498,
+        58192,
+        111340,
+        377201,
+        405163,
+    ],
+}
+
+ESTIMATORS = {
+    "mean": AverageHedges(method="mean", aggressive_mask=False),
+    "median": AverageHedges(method="median", aggressive_mask=False),
+    "trimmed_mean": AverageHedges(method="trimmed", aggressive_mask=False),
+    "winsorized_mean": AverageHedges(method="winsorized", aggressive_mask=False),
+    "fixed_effects": FixedEffectsHedges(aggressive_mask=False),
+}
+
+
+def _get_parser():
+    parser = argparse.ArgumentParser(description="Run IBMA workflow")
+    parser.add_argument(
+        "--project_dir",
+        dest="project_dir",
+        required=True,
+        help="Path to project directory",
+    )
+    parser.add_argument(
+        "--n_cores",
+        dest="n_cores",
+        default=1,
+        required=False,
+        help="CPUs",
+    )
+    return parser
+
+
+def main(project_dir, n_cores=1):
+    project_dir = op.abspath(project_dir)
+    data_dir = op.join(project_dir, "data")
+    image_dir = op.join(data_dir, "neurovault", "images")
+    results_dir = op.join(project_dir, "results", "ibma")
+    n_cores = int(n_cores)
+
+    dset = Dataset.load(op.join(data_dir, "neurovault_full_dataset.pkl"))
+    dset.update_path(image_dir)
+    metadata_df = dset.metadata
+
+    modes = ["full"]
+    # modes = ["full", "auto", "manual"]
+    tasks = ["working_memory"]  # working memory fMRI task paradigm
+    for mode in modes:
+        print(f"Running IBMA for mode: {mode}")
+        for task in tasks:
+            print("Running IBMA for task: ", task)
+
+            task_ids = TASK_IDS[task]
+            sub_metadata_df = metadata_df[
+                metadata_df["cognitive_paradigm_cogatlas_id"].isin(task_ids)
+            ]
+            task_name = sub_metadata_df["cognitive_paradigm_cogatlas_name"].unique()
+
+            print(f"\tTask: {task_name}")
+            dset_task = dset.slice(sub_metadata_df["id"].values)
+            metadata_task_df = dset_task.metadata
+            if mode == "full":
+                ids_sel = metadata_task_df["id"].values
+                metadata_sel_df = metadata_task_df
+
+            elif mode == "auto":
+                # Exclude outliers and non-stat maps
+                dset_task = _rm_nonstat_maps(dset_task)
+                dset_task = _exclude_outliers(dset_task)
+                metadata_sel_df = dset_task.metadata
+
+                # Exclude HCP images
+                # This should be remove by our automtic selection anyway
+                metadata_sel_df = metadata_sel_df[metadata_sel_df["collection_id"] != 457]
+                ids_sel = metadata_sel_df["id"].values
+
+                if len(ids_sel) < 2:
+                    print(f"Skipping task {task} because it has no more than 2 images")
+                    continue
+
+            elif mode == "manual":
+                img_ids = MANUAL_SELECTION[task]
+                metadata_sel_df = metadata_task_df[metadata_task_df["image_id"].isin(img_ids)]
+                ids_sel = metadata_sel_df["id"].values
+
+            else:
+                raise ValueError("Invalid mode")
+
+            n_collections = len(metadata_sel_df["collection_id"].unique())
+            print(f"\t{len(ids_sel)} images selected")
+            print(f"\t{n_collections} collections")
+
+            # Define output directories
+            ibma_dir = op.join(results_dir, task, mode)
+            os.makedirs(ibma_dir, exist_ok=True)
+
+            # Run IBMA on non-HCP
+            dset_task_sel = dset.slice(ids_sel)
+            print(f"\tRunning IBMA, with {len(dset_task_sel.images)} images")
+            for label, estimator in ESTIMATORS.items():
+                print("\t\tUsing estimator: ", label)
+                result_fn = op.join(ibma_dir, f"{label}_result.pkl.gz")
+
+                # Set voxel_thresh to a high value to skip diagnostics for now
+                diagnostics = Jackknife(voxel_thresh=100, n_cores=n_cores)
+                workflow = IBMAWorkflow(
+                    estimator=estimator, diagnostics=diagnostics, n_cores=n_cores
+                )
+                result = workflow.fit(dset_task_sel)
+                result.save(result_fn)
+                print("\t\t\tDone!")
+
+
+def _main(argv=None):
+    option = _get_parser().parse_args(argv)
+    kwargs = vars(option)
+    main(**kwargs)
+
+
+if __name__ == "__main__":
+    _main()
